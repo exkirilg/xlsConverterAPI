a66e11f0339361fa3e600dabe05a053f73c1def0
Kirill <62622495+exkirilg@users.noreply.github.com>
2022-08-19 13:49:56 +0300

    Added rows specification

--- Server/Reading/IReader.cs
+++ Server/Reading/IReader.cs
@@ -2,6 +2,5 @@
 
 public interface IReader
 {
-    Task<DataTable> ReadFileAsync(string filePath, int headerRowOffset, string[] specifiedCols);
-    DataTable ReadFile(string fileName, int headerRowOffset, string[] specifiedCols);
+    Task<DataTable> ReadFileAsync();
 }
--- Server/Reading/Reader.cs
+++ Server/Reading/Reader.cs
@@ -5,23 +5,65 @@ namespace Server.Reading;
 
 public class Reader : IReader
 {
-    public async Task<DataTable> ReadFileAsync(string filePath, int headerRowOffset, string[] specifiedCols)
+    private string _filePath = string.Empty;
+    private string[]? _specifiedColumns = null;
+
+    public string FilePath
     {
-        return await Task.Run(() => ReadFile(filePath, headerRowOffset, specifiedCols));
+        get => _filePath;
+        private set
+        {
+            if (File.Exists(value) == false)
+            {
+                using (File.Create(value)) { };
+                File.Delete(value);
+            }
+
+            _filePath = value;
+        }
     }
-    public DataTable ReadFile(string filePath, int headerRowOffset, string[] specifiedCols)
+    public int HeaderRowOffset { get; private set; }
+    public string[]? SpecifiedColumns
+    {
+        get => _specifiedColumns;
+        private set
+        {
+            if (value is null)
+            {
+                _specifiedColumns = null;
+                return;
+            }
+
+            _specifiedColumns = value.Select(c => c.ToLower().Trim()).ToArray();
+        }
+    }
+    public string? RowsSpecification { get; private set; }
+
+    public Reader(string filePath, int headerRowOffset = 0, string[]? specifiedColumns = null, string? rowsSpecification = null)
+    {
+        FilePath = filePath;
+        HeaderRowOffset = headerRowOffset;
+        SpecifiedColumns = specifiedColumns;
+        RowsSpecification = rowsSpecification;
+    }
+
+    public async Task<DataTable> ReadFileAsync() => await Task.Run(() => ReadFile());
+    
+    private DataTable ReadFile()
     {
         var result = new DataTable();
         var rowList = new List<string>();
 
-        using var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+        using var stream = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
 
         var workbook = new XSSFWorkbook(stream);
         var sheet = workbook.GetSheetAt(0);
-        var headerRow = sheet.GetRow(sheet.FirstRowNum + headerRowOffset);
+
+        var headerRowIndex = sheet.FirstRowNum + HeaderRowOffset;
+        var headerRow = sheet.GetRow(headerRowIndex);
 
         var cellCount = headerRow.LastCellNum;
-
+        
         for (int i = 0; i < cellCount; i++)
         {
             var cell = headerRow.GetCell(i);
@@ -29,14 +71,20 @@ public class Reader : IReader
             if (cell is null || string.IsNullOrEmpty(cell.ToString()))
                 continue;
 
-            if (UploadColumn(cell.ToString()!, specifiedCols) == false)
+            if (UploadColumn(cell.ToString()!) == false)
                 continue;
 
             result.Columns.Add(cell.ToString());
         }
 
-        for (int i = sheet.FirstRowNum + headerRowOffset + 1; i <= sheet.LastRowNum; i++)
+        int firstRowIndex = headerRowIndex + 1;
+        int[] rows = ParseRowsSpecification(firstRowIndex + 1, sheet.PhysicalNumberOfRows);
+
+        for (int i = firstRowIndex; i <= sheet.PhysicalNumberOfRows; i++)
         {
+            if (rows.Contains(i + 1) == false)
+                continue;
+
             var row = sheet.GetRow(i);
 
             if (row is null || row.Cells.All(c => c.CellType == CellType.Blank))
@@ -44,10 +92,10 @@ public class Reader : IReader
 
             for (int j = row.FirstCellNum; j < cellCount; j++)
             {
-                var cell = row.GetCell(j);
-
-                if (UploadColumn(headerRow.GetCell(j).ToString()!, specifiedCols) == false)
+                if (UploadColumn(headerRow.GetCell(j).ToString()!) == false)
                     continue;
+
+                var cell = row.GetCell(j);
 
                 if (cell is null | string.IsNullOrWhiteSpace(cell!.ToString()))
                     continue;
@@ -65,11 +113,47 @@ public class Reader : IReader
         return result;
     }
 
-    private bool UploadColumn(string colName, string[] specifiedCols)
+    private bool UploadColumn(string columnName)
     {
-        if (specifiedCols.Any() == false)
+        if (SpecifiedColumns is null || SpecifiedColumns.Any() == false)
             return true;
 
-        return specifiedCols.Contains(colName.ToLower());
+        return SpecifiedColumns.Contains(columnName.ToLower());
+    }
+
+    private int[] ParseRowsSpecification(int firstRow, int lastRow)
+    {
+        List<int> result = new();
+
+        if (string.IsNullOrWhiteSpace(RowsSpecification))
+            return Enumerable.Range(firstRow, lastRow - firstRow + 1).ToArray();
+
+        foreach (var range in RowsSpecification.Split('|', StringSplitOptions.RemoveEmptyEntries))
+        {
+            int start, end;
+            var separatorPos = range.IndexOf(':');
+
+            if (separatorPos == -1)
+                start = end = int.Parse(range);
+            else
+            {
+                var strStart = range.Substring(0, separatorPos);
+                var strEnd = range.Substring(separatorPos + 1);
+
+                start = string.IsNullOrWhiteSpace(strStart) ? firstRow : int.Parse(strStart);
+                end = string.IsNullOrWhiteSpace(strEnd) ? lastRow : int.Parse(strEnd);
+            }
+
+            if (start < 1)
+                throw new ArgumentException("Starting row cannot be less then 1");
+
+            if (end < start)
+                throw new ArgumentException("Ending row cannot be less then starting row");
+
+            for (var i = start; i <= end; i++)
+                result.Add(i);
+        }
+
+        return result.Distinct().ToArray();
     }
 }
